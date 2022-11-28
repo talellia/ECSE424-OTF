47b83993bfff5e773c03d5651dccbcbf4a93523c
atalel <46718279+talellia@users.noreply.github.com>
2022-11-28 12:28:35 -0500

    all firt pages done

--- src/views/sign-up.js
+++ src/views/sign-up.js
@@ -2,12 +2,48 @@ import React from 'react'
 
 import { Helmet } from 'react-helmet'
 import GamesYouPlaySingUp from '../components/games-you-play-sing-up';
-import Tooltip from '@mui/material/Tooltip';
 import HelpIcon from '@mui/icons-material/Help';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import Box from '@mui/material/Box';
+import { deepPurple } from '@mui/material/colors';
+import { useHistory } from "react-router-dom"
+
 
 import './sign-up.css'
 
 const SignUp = (props) => {
+  const history = useHistory();
+  const SignIn = () => {
+    var password = document.getElementById("password2").value;
+    var repassword = document.getElementById("repassword").value;
+    var errorDic = {
+      passwordISEmpty: password === "",
+      rePasswordIsEmpty: repassword === "",
+      passwordNotSameAsRepassword: password != repassword,
+      userNameMissing: document.getElementById("userName2").value === "",
+      emailMissing: document.getElementById("email").value === ""
+    };
+
+    var errorString = "";
+    for (var key in errorDic) {
+      if (errorDic[key]) {
+        errorString = errorString + key.toString() + "\n";
+      }
+    }
+    if (errorString != "") {
+      alert(errorString);
+    } else {
+      history.push("/home")
+    }
+
+  }
+
+  const game = `Please select all the game you wish to look for teamates.
+  You can always add games later on.`;
+  const link = `Design Update. Next update will allow you to link a stat
+   to the game from the sign in menue. For now you need to add it once signed 
+   in, in the account menue.`;
   return (
     <div className="sign-up-container">
       <Helmet>
@@ -63,13 +99,13 @@ const SignUp = (props) => {
           src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/3970f017-b141-4456-afd4-fb36d956eb37/44387211-67e5-48d8-86b6-965d827826f3?org_if_sml=15189"
           className="sign-up-rectangle3"
         />
-        <input className="sign-up-rectangle9"/>
-        <input type="button" className="sign-up-rectangle10"/>
-        <input className="sign-up-rectangle11"/>
-        <input className="sign-up-rectangle6"/>
-        <input id="password" type="password" className="sign-up-rectangle7"/>
-        <input id="repassword" type="password" className="sign-up-rectangle8"/>
-        
+        <input id="email" className="sign-up-rectangle9" />
+        <input type="button" className="sign-up-rectangle10" />
+        <button className="sign-up-rectangle11">Disabled For Now Please add once signed In</button>
+        <input id="userName2" className="sign-up-rectangle6" />
+        <input id="password2" type="password" className="sign-up-rectangle7" required />
+        <input id="repassword" type="password" className="sign-up-rectangle8" required />
+
         <span className="sign-up-text11">
           <span>User Name</span>
         </span>
@@ -88,9 +124,9 @@ const SignUp = (props) => {
         <span className="sign-up-text21">
           <span>Repeat Password</span>
         </span>
-        
+
         <GamesYouPlaySingUp rootClassName="dropdown"></GamesYouPlaySingUp>
-        
+
         <button className="sign-up-frameiconchevronbottom" onClick={display}>
           <img
             alt="Vector1815"
@@ -98,7 +134,7 @@ const SignUp = (props) => {
             className="sign-up-vector"
           />
         </button>
-        <div className="sign-up-framemodernlarge-base">
+        <button className="sign-up-framemodernlarge-base" onClick={SignIn}>
           <div className="sign-up-content">
             <span className="sign-up-text23">
               <span>SIGN UP</span>
@@ -111,22 +147,51 @@ const SignUp = (props) => {
               />
             </div>
           </div>
-        </div>
-        <div className="sign-up-framemodernlarge-base1">
+        </button>
+        <button className="sign-up-framemodernlarge-base1" onClick={checkGame}>
           <div className="sign-up-content1">
             <span className="sign-up-text25">+</span>
           </div>
-        </div>
-        <Tooltip className='help1' title="Delete">
-            <HelpIcon/>
-        </Tooltip>
+        </button>
+        <Box
+          sx={{
+            '& > :not(style)':
+              { m: 2, },
+            position: 'absolute',
+            top: 615,
+            left: 1120,
+          }}>
+          <Tooltip title={link} placement="right">
+            <IconButton sx={{ color: deepPurple[900] }} fontSize="large">
+              <HelpIcon />
+            </IconButton>
+          </Tooltip>
+        </Box>
+        <Box
+          sx={{
+            '& > :not(style)':
+              { m: 2, },
+            top: 455,
+            left: 1200,
+            position: 'absolute',
+          }}>
+          <Tooltip title={game} placement="right">
+            <IconButton sx={{ color: deepPurple[900] }} fontSize="large">
+              <HelpIcon />
+            </IconButton>
+          </Tooltip>
+        </Box>
       </div>
     </div>
   )
 }
 
+function checkGame() {
+
+}
+
 function display() {
-  document.getElementById("dropdown").style.visibility="visible";
+  document.getElementById("dropdown").style.visibility = "visible";
 }
 
 export default SignUp
